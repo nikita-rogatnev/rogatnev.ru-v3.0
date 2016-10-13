a2512705e13076ed5dd5d0864452cb030477ad35
Rogatnev Nikita <mail@rogatnev.ru>
2016-10-13 18:00:37 +0700

    contact page

--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -1,6 +1,5 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
-import {SocialIcon} from 'react-social-icons';
 
 const propTypes = {
     children: PropTypes.element
@@ -17,17 +16,6 @@ function contact({children}) {
                         </svg>
                         <h1>Contact me</h1>
                     </div>
-                    <div className="contact__social">
-                        <div className="title">
-                            <h2>
-                                <span>Let's socialize</span>
-                            </h2>
-                        </div>
-                        <SocialIcon className="contact__social-item" url="https://vk.com/rogatnev_nikita" network="vk" color="#1c262b" />
-                        <SocialIcon className="contact__social-item" url="https://github.com/rogatnev-nikita" network="github" color="#1c262b" />
-                        <SocialIcon className="contact__social-item" url="https://linkedin.com/in/rogatnevnikita/" network="linkedin" color="#1c262b" />
-                        <SocialIcon className="contact__social-item" url="https://instagram.com/rogatnev_nikita/" network="instagram" color="#1c262b" />
-                    </div>
                     <div className="contact__reach">
                         <div className="title">
                             <h2>
@@ -36,30 +24,61 @@ function contact({children}) {
                         </div>
                         <div className="contact__reach-item">
                             <svg>
-                                <use xlinkHref="#pin"></use>
+                                <use xlinkHref="#map-marker"></use>
                             </svg>
-                            <h3>Koh Samui, Thailand</h3>
+                            <h3>Location<br />Koh Samui</h3>
                         </div>
                         <div className="contact__reach-item">
                             <svg>
                                 <use xlinkHref="#call"></use>
                             </svg>
-                            <h3>Tel: +7 999 216-37-96</h3>
+                            <h3>Skype<br /><a href="skype:rogatnev_nikita?chat">rogatnev_nikita</a></h3>
                         </div>
                         <div className="contact__reach-item">
                             <svg>
                                 <use xlinkHref="#mail"></use>
                             </svg>
-                            <h3>mail [at] rogatnev.ru</h3>
+                            <h3>Email<br /><a href="mailto:mail@rogatnev.ru">mail@rogatnev.ru</a></h3>
                         </div>
                         <div className="contact__reach-item">
                             <svg>
-                                <use xlinkHref="#tick"></use>
+                                <use xlinkHref="#check"></use>
                             </svg>
-                            <h3>Freelance available</h3>
+                            <h3>Freelance<br />Available</h3>
                         </div>
                     </div>
-
+                    <div className="contact__social">
+                        <div className="title">
+                            <h2>
+                                <span>Let's socialize</span>
+                            </h2>
+                        </div>
+                        <a className="contact__social-item" target="_blank" href="https://vk.com/rogatnev_nikita">
+                            <svg>
+                                <use xlinkHref="#vk-logo"></use>
+                            </svg>
+                        </a>
+                        <a className="contact__social-item" target="_blank" href="https://github.com/rogatnev-nikita">
+                            <svg>
+                                <use xlinkHref="#github-logo"></use>
+                            </svg>
+                        </a>
+                        <a className="contact__social-item" target="_blank" href="https://linkedin.com/in/rogatnevnikita/">
+                            <svg>
+                                <use xlinkHref="#in-logo"></use>
+                            </svg>
+                        </a>
+                        <a className="contact__social-item" target="_blank" href="https://instagram.com/rogatnev_nikita/">
+                            <svg>
+                                <use xlinkHref="#inst-logo"></use>
+                            </svg>
+                        </a>
+                        <a className="contact__social-item" target="_blank" href="https://www.facebook.com/rogatnevnikita">
+                            <svg>
+                                <use xlinkHref="#fb-logo"></use>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
             </section>
         </div>
