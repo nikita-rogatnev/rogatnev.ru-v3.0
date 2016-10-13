989cb0e922022d5438189876f02ad9a3e921bed2
Rogatnev Nikita <mail@rogatnev.ru>
2016-10-13 14:53:57 +0700

    init social links

--- src/components/home/about.jsx
+++ src/components/home/about.jsx
@@ -10,7 +10,100 @@ function about({children}) {
         <div className="main__container">
             <section className="about" id="about">
                 <div className="about__container">
-                    about
+                    <div className="about__heading">
+                        <svg>
+                            <use xlinkHref="#user"></use>
+                        </svg>
+                        <h1>About me</h1>
+                    </div>
+                    <div className="about__services">
+                        <div className="title">
+                            <h2>
+                                <span>Services</span>
+                            </h2>
+                        </div>
+                        <div className="about__services-item">
+
+                        </div>
+                        <div className="about__services-item">
+
+                        </div>
+                        <div className="about__services-item">
+
+                        </div>
+                        <div className="about__services-item">
+
+                        </div>
+                    </div>
+                    <div className="about__process">
+                        <div className="title">
+                            <h2>
+                                <span>Work process</span>
+                            </h2>
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                        <div className="about__process-item">
+
+                        </div>
+                    </div>
+                    <div className="about__clients">
+                        <div className="title">
+                            <h2>
+                                <span>Clients</span>
+                            </h2>
+                        </div>
+                        <div className="about__clients-item">
+                            <img src="/build/img/about/__clients/rwm.png" alt="" width="160" height="160" />
+                        </div>
+                        <div className="about__clients-item">
+
+                        </div>
+                        <div className="about__clients-item">
+
+                        </div>
+                        <div className="about__clients-item">
+
+                        </div>
+                        <div className="about__clients-item">
+
+                        </div>
+                        <div className="about__clients-item">
+
+                        </div>
+                    </div>
+                    <div className="about__facts">
+                        <div className="title">
+                            <h2>
+                                <span>Fun fact</span>
+                            </h2>
+                        </div>
+                        <div className="about__facts-item">
+
+                        </div>
+                        <div className="about__facts-item">
+
+                        </div>
+                        <div className="about__facts-item">
+
+                        </div>
+                        <div className="about__facts-item">
+
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
--- src/components/home/contact.jsx
+++ src/components/home/contact.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
+import {SocialIcon} from 'react-social-icons';
 
 const propTypes = {
     children: PropTypes.element
@@ -10,7 +11,55 @@ function contact({children}) {
         <div className="main__container">
             <section className="contact" id="contact">
                 <div className="contact__container">
-                    contact
+                    <div className="contact__heading">
+                        <svg>
+                            <use xlinkHref="#user"></use>
+                        </svg>
+                        <h1>Contact me</h1>
+                    </div>
+                    <div className="contact__social">
+                        <div className="title">
+                            <h2>
+                                <span>Let's socialize</span>
+                            </h2>
+                        </div>
+                        <SocialIcon className="contact__social-item" url="https://vk.com/rogatnev_nikita" network="vk" color="#1c262b" />
+                        <SocialIcon className="contact__social-item" url="https://github.com/rogatnev-nikita" network="github" color="#1c262b" />
+                        <SocialIcon className="contact__social-item" url="https://linkedin.com/in/rogatnevnikita/" network="linkedin" color="#1c262b" />
+                        <SocialIcon className="contact__social-item" url="https://instagram.com/rogatnev_nikita/" network="instagram" color="#1c262b" />
+                    </div>
+                    <div className="contact__reach">
+                        <div className="title">
+                            <h2>
+                                <span>Reach me</span>
+                            </h2>
+                        </div>
+                        <div className="contact__reach-item">
+                            <svg>
+                                <use xlinkHref="#pin"></use>
+                            </svg>
+                            <h3>Koh Samui, Thailand</h3>
+                        </div>
+                        <div className="contact__reach-item">
+                            <svg>
+                                <use xlinkHref="#call"></use>
+                            </svg>
+                            <h3>Tel: +7 999 216-37-96</h3>
+                        </div>
+                        <div className="contact__reach-item">
+                            <svg>
+                                <use xlinkHref="#mail"></use>
+                            </svg>
+                            <h3>mail [at] rogatnev.ru</h3>
+                        </div>
+                        <div className="contact__reach-item">
+                            <svg>
+                                <use xlinkHref="#tick"></use>
+                            </svg>
+                            <h3>Freelance available</h3>
+                        </div>
+                    </div>
+
                 </div>
             </section>
         </div>
