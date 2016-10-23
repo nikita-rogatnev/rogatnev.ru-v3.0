0b380881ad9922918de7e7b5a26469c4c18e13df
Rogatnev Nikita <mail@rogatnev.ru>
2016-10-23 12:14:22 +0700

    add build

---
+++ build/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    // ------------------------------------------ //
+    // MOBILE MENU
+    // ------------------------------------------ //
+    $(".header__mobile .header__mobile-toggle").click(function(e) {
+        e.preventDefault();
+        $(".header__mobile-icon").toggleClass("header__mobile-icon--active");
+        $(".header").toggleClass("header--active");
+        $(".main").toggleClass("main--active");
+    });
+
+});
