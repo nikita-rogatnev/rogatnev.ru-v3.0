860d8fdbbb8743fd39521a859dc8535f3f943f46
Rogatnev Nikita <mail@rogatnev.ru>
2016-12-18 12:10:10 +0800

    FU jquery

--- build/js/script.js
+++ build/js/script.js
@@ -1,60 +1,58 @@
-$(document)
-	.ready(function () {
+// ------------------------------------------ //
+// MOBILE MENU
+// ------------------------------------------ //
+var menuToggle = document.querySelector("#header__mobile-toggle");
+var menuClose = document.querySelector("#header__mobile-close");
+var menuOpen = document.querySelector("#header__mobile-open");
+var header = document.querySelector("#header");
+var main = document.querySelector("#main");
 
-		// ------------------------------------------ //
-		// CERTIFICARES MODALS
-		// ------------------------------------------ //
-		$(".certificates__item")
-			.click(function () {
-				$(this)
-					.children(".certificates__modal")
-					.toggleClass("certificates__modal--active");
-			});
+menuToggle.addEventListener("click", function (event) {
+	event.preventDefault();
+	if (menuOpen.classList.contains("header__mobile-icon--active")) {
+		menuClose
+			.classList
+			.add("header__mobile-icon--active");
+		menuOpen
+			.classList
+			.remove("header__mobile-icon--active");
+		header
+			.classList
+			.add("header--active");
+		main
+			.classList
+			.add("main--active");
+	} else {
+		menuClose
+			.classList
+			.remove("header__mobile-icon--active");
+		menuOpen
+			.classList
+			.add("header__mobile-icon--active");
+		header
+			.classList
+			.remove("header--active");
+		main
+			.classList
+			.remove("main--active");
+	};
+});
 
-		$(document).keydown(function (e) {
-			if (e.keyCode == 27) {
-				$(".certificates__modal").removeClass("certificates__modal--active");
-			}
-		});
-
-		// ------------------------------------------ //
-		// MOBILE MENU
-		// ------------------------------------------ //
-		var menuToggle = document.querySelector("#header__mobile-toggle");
-		var menuClose = document.querySelector("#header__mobile-close");
-		var menuOpen = document.querySelector("#header__mobile-open");
-		var header = document.querySelector("#header");
-		var main = document.querySelector("#main");
-
-		menuToggle.addEventListener("click", function (event) {
-			event.preventDefault();
-			if (menuOpen.classList.contains("header__mobile-icon--active")) {
-				menuClose
-					.classList
-					.add("header__mobile-icon--active");
-				menuOpen
-					.classList
-					.remove("header__mobile-icon--active");
-				header
-					.classList
-					.add("header--active");
-				main
-					.classList
-					.add("main--active");
-			} else {
-				menuClose
-					.classList
-					.remove("header__mobile-icon--active");
-				menuOpen
-					.classList
-					.add("header__mobile-icon--active");
-				header
-					.classList
-					.remove("header--active");
-				main
-					.classList
-					.remove("main--active");
-			};
-		});
-
-	});
+// ------------------------------------------ //
+// CERTIFICARES MODALS
+// ------------------------------------------ //
+var certificates__item = document.querySelectorAll(".certificates__item");
+for (var i = 0; i < certificates__item.length; i++) {
+	var certificate = certificates__item[i];
+	certificate.onclick = function () {
+		if (this.classList.contains("certificates__item--active")) {
+			this
+				.classList
+				.remove("certificates__item--active");
+		} else {
+			this
+				.classList
+				.add("certificates__item--active");
+		}
+	};
+}
